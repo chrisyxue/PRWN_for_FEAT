8e046aa2b62a38bd584e80e21c888d17c3ecf089
ChrisXue <41327917+chrisyxue@users.noreply.github.com>
2020-09-17 15:06:28 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,5 @@
+import torch
+
+log = torch.load("/opt/data/private/FEAT-master/data/checkpoints/MiniImageNet-FEAT-ConvNet-05w01s15q-Pre-DIS/20_0.5_lr0.0001mul10_step_T164.0T216.0_b1.0_bsz080-NoAug/trlog")
+
+print(log)
